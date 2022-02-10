fb8f5137bb6f4506e620899875a284f03e51476e
Joshua Nario <joshuanario@users.noreply.github.com>
2022-02-09 23:48:58 -0500

    mobile fixes

--- pages/about_us.tsx
+++ pages/about_us.tsx
@@ -9,12 +9,12 @@ import missionImg from '../public/mission.png'
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div className={styles.padded}>
-            <h1>Our Mission</h1>
-            <p>Enable the proliferation of regulatory disclosure technology in order to help combat climate change and to aid with the eradication of forced labor throughout the world.</p>
-            <div>
-              <Image src={missionImg} alt='lightbulb filled with small green plant in burnt and dried soil'></Image>
-            </div>
+      <div className={styles.mission}>
+        <h1>Our Mission</h1>
+        <p>Enable the proliferation of regulatory disclosure technology in order to help combat climate change and to aid with the eradication of forced labor throughout the world.</p>
+      </div>
+      <div className={styles.image}>
+        <Image src={missionImg} alt='lightbulb filled with small green plant in burnt and dried soil'></Image>
       </div>
     </Layout>
   )
