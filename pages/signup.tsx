278e9c1a6aa5613726d15c36b3f51273d9f2602e
Joshua Nario <joshuanario@users.noreply.github.com>
2022-02-09 19:43:27 -0500

    signup page fixes

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -47,7 +47,7 @@ const SignUp: NextPage<Props> = ({csrfToken} : Props) => {
     if (submit) {
         return (
             <Layout>
-                <div className='padded'>
+                <div className={styles.padded}>
                     <p>
                         Signing up in progress...
                     </p>
@@ -58,75 +58,80 @@ const SignUp: NextPage<Props> = ({csrfToken} : Props) => {
     if (processed) {
         return (
             <Layout>
-                <div className='padded'>
-                    <h1>
-                        Thank you.
-                    </h1>
-                    <p>
-                        Feel free to contact us at <a href="mailto:contact@ecksbee.com">contact@ecksbee.com</a>.
-                    </p>
-                    <p className={styles.home}><Link href='/'>Go home</Link></p>
+                <div className={styles.padded}>
+                    <div className={styles.scroll}>
+                        <h1>
+                            Thank you.
+                        </h1>
+                        <p>
+                            Feel free to contact us at <a href="mailto:contact@ecksbee.com">contact@ecksbee.com</a>.
+                        </p>
+                        <p className={styles.home}><Link href='/'>Go home</Link></p>
+                    </div>
                 </div>
             </Layout>
         )
     }
     return (
         <Layout>
-            <div className='padded'>
-                <h1>Want to hear from us?</h1>
-                <h2>Tell us about yourself and sign up.</h2>
-                <form onSubmit={async e => await signup(e)}>
-                    <div>
-                        <label htmlFor="firstName">First Name</label>
-                    </div>
-                    <div>
-                        <input className={styles.fullWidth} id="firstName" name="firstName" type="text" autoComplete="given-name" required />
-                    </div>
+            <div className={styles.padded}>
+                <div className={styles.scroll}>
+                    <h1>Want to hear from us?</h1>
+                    <h2>Tell us about yourself and sign up.</h2>
+                    <form onSubmit={async e => await signup(e)}>
+                        <div>
+                            <label htmlFor="firstName">First Name</label>
+                        </div>
+                        <div>
+                            <input className={styles.fullWidth} id="firstName" name="firstName" type="text" autoComplete="given-name" required />
+                        </div>
 
-                    <div>
-                        <label htmlFor="lastName">Last Name</label>
-                    </div>
-                    <div>
-                        <input className={styles.fullWidth} id="lastName" name="lastName" type="text" autoComplete="family-name" required />
-                    </div>
+                        <div>
+                            <label htmlFor="lastName">Last Name</label>
+                        </div>
+                        <div>
+                            <input className={styles.fullWidth} id="lastName" name="lastName" type="text" autoComplete="family-name" required />
+                        </div>
 
-                    <div>
-                        <label htmlFor="phone">Phone Number</label>
-                    </div>
-                    <div>
-                        <input className={styles.fullWidth} id="phone" name="phone" type="text" autoComplete="tel" required />
-                    </div>
+                        <div>
+                            <label htmlFor="phone">Phone Number</label>
+                        </div>
+                        <div>
+                            <input className={styles.fullWidth} id="phone" name="phone" type="text" autoComplete="tel" required />
+                        </div>
 
-                    <div>
-                        <label htmlFor="email">Email Address</label>
-                    </div>
-                    <div>
-                        <input className={styles.fullWidth} id="email" name="email" type="text" autoComplete="email" required />
-                    </div>
-                    
-                    <div>
-                        <label htmlFor="reason">Why are you signing up?</label>
-                    </div>
-                    <div>
-                        <textarea className={styles.fullWidth} id="reason" name="reason" rows={10} required />
-                    </div>
+                        <div>
+                            <label htmlFor="email">Email Address</label>
+                        </div>
+                        <div>
+                            <input className={styles.fullWidth} id="email" name="email" type="text" autoComplete="email" required />
+                        </div>
+                        
+                        <div>
+                            <label htmlFor="reason">Why are you signing up?</label>
+                        </div>
+                        <div>
+                            <textarea className={styles.fullWidth} id="reason" name="reason" rows={10} required />
+                        </div>
 
-                    <HCaptcha sitekey={siteKey} onVerify={
-                        async (token: string) => {
-                            await fetch('/api/hcaptcha', {
-                              body: JSON.stringify({
-                                  response: token
-                              }),
-                              method: 'POST',
-                              credentials: 'include',
-                            })
-                            setHcaptcha(true)
-                        }
-                    }/>
-                    <div>
-                        <button className={hcaptcha? styles.btnSubmit : styles.btnDisabled} type={hcaptcha? 'submit' : 'button'}>Sign me up</button>
-                    </div>
-                </form>
+                        <HCaptcha sitekey={siteKey} onVerify={
+                            async (token: string) => {
+                                await fetch('/api/hcaptcha', {
+                                body: JSON.stringify({
+                                    response: token
+                                }),
+                                method: 'POST',
+                                credentials: 'include',
+                                })
+                                setHcaptcha(true)
+                            }
+                        }/>
+                        <div>
+                            {hcaptcha && <button className={styles.btnSubmit} type='submit'>Sign me up</button>}
+                            {!hcaptcha && <button className={styles.btnDisabled} type='button'><s>Sign me up</s></button>}
+                        </div>
+                    </form>
+                </div>
             </div>
         </Layout>
     )
