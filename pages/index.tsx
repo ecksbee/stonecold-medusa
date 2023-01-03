4ce0ed39c8a7ff072d0d435bc138fdfdf8d0410b
Joshua Nario <joshuanario@users.noreply.github.com>
2023-01-02 19:51:08 -0500

    hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import type { NextPage } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
-import heroImg from '../public/hero.png'
+import beesImg from '../public/bees.jpg'
 
-const hero = <div className={styles.carousel} >
-  
+const hero = <div className={styles.hero}>
+  <div className={styles.heroImg} >
+    <Image src={beesImg} alt='bees honeycomb' fill/>
+  </div>
+  <div>
+    <h1>EDGARBrowser is here</h1>
+    <p>Browse XBRL files within EDGAR that has been accepted by the SEC.  XBRL rendering is powered by TeleFacts, an ECKSBEE software. <span className={styles.cta}><Link href='edgarbrowser'>Go</Link></span></p>
+  </div>
 </div>
 const Home: NextPage = () => {
   return (
