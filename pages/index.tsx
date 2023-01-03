25bececd0ad42341799533aca59ed986d0003b21
Joshua Nario <joshuanario@users.noreply.github.com>
2023-01-02 19:55:11 -0500

    hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const hero = <div className={styles.hero}>
   <div className={styles.heroImg} >
     <Image src={beesImg} alt='bees honeycomb' fill/>
   </div>
-  <div>
+  <div className={styles.heroBanner}>
     <h1>EDGARBrowser is here</h1>
     <p>Browse XBRL files within EDGAR that has been accepted by the SEC.  XBRL rendering is powered by TeleFacts, an ECKSBEE software. <span className={styles.cta}><Link href='edgarbrowser'>Go</Link></span></p>
   </div>
